faad381a46a63f3d0f385b6bd8685a7dcf831c65
Erga1 <irgabuushirpha@gmail.com>
2024-12-26 16:41:00 +0300

    Server and Client connection using socket

---
+++ server client/src/client.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class client {
+    public static void main(String[] args) {
+        DataInputStream in;
+        DataOutputStream out;
+        String resp;
+        String req;
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            Socket sok=new Socket("localhost",12345);
+            System.out.println("Connection Successful");
+            in=new DataInputStream(sok.getInputStream());
+            out=new DataOutputStream(sok.getOutputStream());
+            while(true) {
+
+
+                System.out.print("You:- ");
+                req=sc.nextLine();
+                out.writeUTF(req);
+                resp = in.readUTF();
+
+                if ( resp.substring(0,resp.length()-9).equals("exit")) {
+                    break;
+                }
+                System.out.println("server: "+resp);
+
+            }
+
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+
+
+    }
+}
---
+++ server client/src/server.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.*;
+
+public class server {
+    public static void main(String[] args) {
+        DataInputStream in;
+        DataOutputStream out;
+        String resp;
+
+        try{
+            ServerSocket sok=new ServerSocket(12345);
+            Socket so=sok.accept();
+            System.out.println("Connection Successful");
+            in=new DataInputStream(so.getInputStream());
+            out=new DataOutputStream(so.getOutputStream());
+            while(true) {
+                resp = in.readUTF();
+                if (resp.equals("exit")) {
+                    break;
+                }
+                System.out.println("Client: "+resp);
+                out.writeUTF(resp+" recieved");
+            }
+
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+
+
+    }
+}
